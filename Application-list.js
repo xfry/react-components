5fd85bb417912d4c6993a20ec0d26dea9fec2391
xfry <fry.andrade@gmail.com>
2017-03-20 12:01:55 -0500

    adding Application list component

---
+++ Application-list.js
@@ -0,0 +1,20 @@
+// src/compoents/Application-list.js
+import React from "react";
+
+class ApplicationList extends React.Component{
+  comstructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>PCTE canvas</li>
+        </ul>
+      </div>
+    );
+  }
+};
+
+export default ApplicationList;
